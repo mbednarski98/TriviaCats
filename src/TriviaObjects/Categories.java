d27d0df10814c0bc6d36ca63b588700e666ed45c
Christopher John <christopherjohn00@protonmail.com>
2019-10-29 00:37:40 -0400

    added wrapper functions for ArrayLists in Questions and Categories

--- src/TriviaObjects/Categories.java
+++ src/TriviaObjects/Categories.java
@@ -5,7 +5,12 @@ import java.util.ArrayList;
 public class Categories {
 	private ArrayList<Category> trivia_categories;
 	
-	public ArrayList<Category> getCategories() {
-		return this.trivia_categories;
+	public int getNumberOfCategories() {
+		return this.trivia_categories.size();
 	}
+	
+	public Category getCategory(int index) {
+		return this.trivia_categories.get(index);
+	}
+	
 }
--- src/TriviaObjects/Questions.java
+++ src/TriviaObjects/Questions.java
@@ -3,10 +3,14 @@ package TriviaObjects;
 import java.util.ArrayList;
 
 public class Questions {
-	
 	private ArrayList<Question> results;
 	
-	public ArrayList<Question> getQuestions() {
-		return this.results;
+	public int getNumberOfQuestions() {
+		return results.size();
 	}
+	
+	public Question getQuestion(int index) {
+		return this.results.get(index);
+	}
+	
 }
