437a0b5822ae9627279154690a1e036e52d94207
Christopher John <christopherjohn00@protonmail.com>
2019-10-30 15:21:40 -0400

    added descriptions for Questions object as well as each method in comments

--- src/TriviaObjects/Questions.java
+++ src/TriviaObjects/Questions.java
@@ -2,13 +2,17 @@ package TriviaObjects;
 
 import java.util.ArrayList;
 
+/* This object holds a list of Question objects. It is used in the same way as an ArrayList,
+   as it is essentially a case-specific wrapper for ArrayList. */
 public class Questions {
 	private ArrayList<Question> results;
 	
+	// Returns number of Question objects in the list.
 	public int getNumberOfQuestions() {
 		return this.results.size();
 	}
 	
+	// Takes in an index, returns the Question object at that index.
 	public Question getQuestion(int index) {
 		return this.results.get(index);
 	}
