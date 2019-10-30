9fdbdf937f845178f6faf90befc60f5b64252d8a
Christopher John <christopherjohn00@protonmail.com>
2019-10-30 15:13:00 -0400

    added descriptions for Catagories objest as well as each method in comments

--- src/TriviaObjects/Categories.java
+++ src/TriviaObjects/Categories.java
@@ -2,13 +2,17 @@ package TriviaObjects;
 
 import java.util.ArrayList;
 
+// This object holds a list of Category objects. It is used in the same way as an ArrayList,
+// as it is essentially a case-specific wrapper for ArrayList.
 public class Categories {
 	private ArrayList<Category> trivia_categories;
 	
+	// Returns the number of elements in trivia_categories.
 	public int getNumberOfCategories() {
 		return this.trivia_categories.size();
 	}
 	
+	// Take in an index, and returns the Category object at that index.
 	public Category getCategory(int index) {
 		return this.trivia_categories.get(index);
 	}
