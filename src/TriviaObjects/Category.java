77ebbd65c2e99bf76f5241e4c9ddf19ecc2d8164
Christopher John <christopherjohn00@protonmail.com>
2019-10-30 15:26:42 -0400

    added descriptions for Question object as well as each method in comments

--- src/TriviaObjects/Category.java
+++ src/TriviaObjects/Category.java
@@ -3,9 +3,8 @@ package TriviaObjects;
 // This object holds the information needed to grab questions of a specific category from the Open Trivia DB.
 public class Category {
 
-	private int id;			/* Holds the id of the category.
-							   This is used by Open Trivia DB when using the category parameter in the API request. */
-	private String name;	// Holds the title of the category.
+	private int id;			// This is used by Open Trivia DB when using the category parameter in the API request.
+	private String name;
 	
 	// Returns the Category ID.
 	public int getId() {
--- src/TriviaObjects/Question.java
+++ src/TriviaObjects/Question.java
@@ -1,5 +1,6 @@
 package TriviaObjects;
 
+// This Object holds all the information needed to ask a question.
 public class Question {
 
 	private String category;
@@ -9,26 +10,32 @@ public class Question {
 	private String correct_answer;
 	private String[] incorrect_answers;
 	
+	// Returns category of the question.
 	public String getCategory() {
 		return this.category;
 	}
 	
+	// Returns the type of question. Either "boolean" for True/False, or "multiple" for multiple choice.
 	public String getType() {
 		return this.type;
 	}
 	
+	// Returns the difficulty of the question. Either "easy", "medium", or "hard".
 	public String getDifficulty() {
 		return this.difficulty;
 	}
 	
+	// Returns the question text.
 	public String getQuestion() {
 		return this.question;
 	}
 	
+	// Returns the correct answer.
 	public String getCorrectAnswer() {
 		return this.correct_answer;
 	}
 	
+	// Returns a list of incorrect answer(s).
 	public String[] getIncorrectAnswers() {
 		return this.incorrect_answers;
 	}
