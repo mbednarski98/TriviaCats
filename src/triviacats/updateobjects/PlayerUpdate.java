4fa69aaf84755d1b4cb6448279d8af23e6b16fca
Christopher John <christopherjohn00@protonmail.com>
2019-12-02 10:42:27 -0500

    added constructor to PlayerUpdate

--- src/triviacats/updateobjects/PlayerUpdate.java
+++ src/triviacats/updateobjects/PlayerUpdate.java
@@ -1,9 +1,16 @@
 package triviacats.updateobjects;
 
+import triviacats.websocket.Player;
+
 public class PlayerUpdate {
 
 	String player_id;
 	String player_name;
 	String player_state;
 
+	public PlayerUpdate(Player p, String playerState) {
+		this.player_id = p.getSessionID();
+		this.player_name = p.getName();
+		this.player_state = playerState;
+	}
 }
